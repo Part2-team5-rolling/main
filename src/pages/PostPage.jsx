d5761005c8dce5df697552c515465c3096b75dc0
Dong-Han-Kim <dh32110@gmail.com>
2025-04-18 15:12:34 +0900

    Chore: 페이지 상 데이터 불러오기 Hook으로 수정

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,22 +1,42 @@
 import { useParams } from 'react-router-dom';
 import PostHeader from '../components/Layout/post-components/PostHeader';
 import { getRecipientsData } from '../api/recipients-api';
-
-const data = await getRecipientsData();
+import { useEffect, useState } from 'react';
 
 const PostPage = () => {
+	const [data, setData] = useState({});
+	const [load, setLoad] = useState(true);
 	const { id } = useParams();
-	const [result] = data.results.filter((user) => user.id === Number(id));
 
-	const { name, backgroundColor, backgroundImageURL, messageCount, recentMessages, topReactions } = result;
+	useEffect(() => {
+		const postDataCall = async () => {
+			try {
+				const dataCall = await getRecipientsData();
+				const [filterUser] = dataCall.results.filter((user) => user.id === Number(id));
+				setData(filterUser);
+			} catch (error) {
+				console.error('Post page 데이터 불러오기 실패!', error);
+			} finally {
+				setLoad(false);
+			}
+		};
+		postDataCall();
+	}, []);
+
+	const { name, backgroundColor, backgroundImageURL, messageCount, recentMessages, topReactions } = data;
 	return (
 		<>
-			<PostHeader
-				userName={name}
-				messageCount={messageCount}
-				recentMessage={recentMessages}
-				topReactions={topReactions}
-			/>
+			{load ? (
+				<h1>로딩중...</h1>
+			) : (
+				<PostHeader
+					userName={name}
+					messageCount={messageCount}
+					recentMessage={recentMessages}
+					topReactions={topReactions}
+				/>
+			)}
+
 			<div>Post Page</div>
 		</>
 	);
