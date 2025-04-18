40a258dc4427790faf568fc8bde9b62291864164
Habist <31752426+Habist@users.noreply.github.com>
2025-04-18 15:21:37 +0900

    feat : 메시지 생성 폼 컴포넌트 추가
    * feat : 메시지 생성 폼 컴포넌트 추가
    
    * feat : reciipient id props 추가
    
    * chore : console log 제거

---
+++ src/api/messages-api.js
@@ -0,0 +1,28 @@
+const TEAM = "15-5";
+const BASE_URL = `https://rolling-api.vercel.app/${TEAM}/recipients`;
+const HEADERS = {
+  "Content-Type": "application/json",
+};
+
+
+export async function createMessage(recipientId, { from, profileImageURL, relationship, content, font}) {
+  const response = await fetch(`${BASE_URL}/${recipientId}/messages/`,
+    {
+      method: "POST",
+      headers: HEADERS,
+      body: JSON.stringify(
+        {
+          team: TEAM,
+          recipientId: recipientId,
+          sender: from,
+          profileImageURL,
+          relationship,
+          content,
+          font,
+        }
+      )
+    }
+  );
+  const body = await response.json();
+  return body;
+}
