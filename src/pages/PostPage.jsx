45d6ad02da3f311b228e15aa5dd6cbc5078a81f8
Dong Han <148463611+Dong-Han-Kim@users.noreply.github.com>
2025-04-21 14:25:56 +0900

    Feat: post 페이지 무한 스크롤 구현 (#11)
    * Feat: Post page의 header 컴포넌트 생성 및 recipients api 호출
    
    * Chore: api 호출 파일명 recipients-api로 변경 및 호출 함수명 getRecipientsData으로 변경
    
    * Chore: 페이지 상 데이터 불러오기 Hook으로 수정
    
    * Chore: 머지 된 변경 사항 적용
    
    * Feat: edit 페이지로 이동 기능 추가
    
    * Feat: 무한 스크롤 기능 구현
    
    * Chore: 들여쓰기 설정 변경

--- src/pages/PostPage.jsx
+++ src/pages/PostPage.jsx
@@ -1,32 +1,84 @@
 import { useParams } from 'react-router-dom';
 import PostHeader from '../components/Layout/post-components/PostHeader';
-import { getRecipientsData } from '../api/recipients-api';
-import { useEffect, useState } from 'react';
+import { getRecipientsData, getRecipientsMessage } from '../api/recipients-api';
+import { useEffect, useRef, useState } from 'react';
+import style from '../styles/PostPage.module.css';
+import Card from '../components/Card';
 
 const PostPage = () => {
 	const [data, setData] = useState({});
-	const [load, setLoad] = useState(true);
+	const [headerLoad, setHeaderLoad] = useState(true);
+	const [loading, setLoading] = useState(false);
+	const [messages, setMessages] = useState([]);
+	const [nextLoad, setNextLoad] = useState('');
+	const [offset, setOffset] = useState(0);
+	const [limit, setLimit] = useState(5);
 	const { id } = useParams();
+	const { name, backgroundColor, backgroundImageURL, messageCount, recentMessages, topReactions } = data;
+	const background = backgroundColor ? { backgroundColor } : { backgroundImageURL };
+	const targetRef = useRef(null);
 
+	const loadMore = () => {
+		setOffset((prev) => prev + 5);
+		setLimit((prev) => prev + 6);
+	};
+
+	// 롤링 페이퍼 기본 데이터 불러오기
 	useEffect(() => {
 		const postDataCall = async () => {
 			try {
-				const dataCall = await getRecipientsData();
-				const [filterUser] = dataCall.results.filter((user) => user.id === Number(id));
-				setData(filterUser);
+				const dataCall = await getRecipientsData(id);
+				setData(dataCall);
+				console.log('mounting');
 			} catch (error) {
 				console.error('Post page 데이터 불러오기 실패!', error);
 			} finally {
-				setLoad(false);
+				setHeaderLoad(false);
 			}
 		};
 		postDataCall();
-	}, []);
+	}, [id]);
 
-	const { name, backgroundColor, backgroundImageURL, messageCount, recentMessages, topReactions } = data;
+	// 해당 롤링 페이퍼에 보내진 메세지 가져오기
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const { next, results } = await getRecipientsMessage(id, offset, limit);
+				const addNewMessage = [...messages, ...results];
+				const filterMessage = addNewMessage.filter((item, i, arr) => i === arr.findIndex((obj) => obj.id === item.id));
+				setMessages(filterMessage);
+				setNextLoad(next);
+			} catch (error) {
+				console.error('messages 불러오기 실패', error);
+			} finally {
+				setLoading(true);
+			}
+		};
+		fetchData();
+	}, [id, offset, limit]);
+
+	// 무한 스크롤 IntersectionObserver
+	useEffect(() => {
+		if (loading && nextLoad) {
+			const observer = new IntersectionObserver(
+				(entries) => {
+					if (entries[0].isIntersecting) {
+						loadMore();
+					}
+				},
+				{ threshold: 1 }
+			);
+			observer.observe(targetRef.current);
+
+			return () => {
+				observer.disconnect();
+			};
+		}
+	}, [loading, nextLoad]);
+
 	return (
 		<>
-			{load ? (
+			{headerLoad ? (
 				<h1>로딩중...</h1>
 			) : (
 				<PostHeader
@@ -37,7 +89,32 @@ const PostPage = () => {
 				/>
 			)}
 
-			<div>Post Page</div>
+			<section className={style.post__content} style={background}>
+				<div className={style.card__wrap}>
+					<Card userId={id} />
+					{messageCount === 0 ? (
+						<h3>불러올 메세지가 없습니다.</h3>
+					) : (
+						messages.map((message) => {
+							return (
+								<Card
+									key={message.id}
+									id={message.id}
+									sender={message.sender}
+									relationship={message.relationship}
+									profileImg={message.profileImageURL}
+									cardFont={message.font}
+									content={message.content}
+									createdAt={message.createdAt}
+								/>
+							);
+						})
+					)}
+				</div>
+				<div className={style.ovserver__target} ref={targetRef}>
+					...로딩중
+				</div>
+			</section>
 		</>
 	);
 };
