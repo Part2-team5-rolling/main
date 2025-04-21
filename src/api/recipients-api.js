254a627bc3f63652fdbbf6987f67f3d1afe69323
Dong-Han-Kim <dh32110@gmail.com>
2025-04-21 13:41:10 +0900

    Feat: 무한 스크롤 기능 구현

--- src/api/recipients-api.js
+++ src/api/recipients-api.js
@@ -1,25 +1,29 @@
-const BASE_URL = 'https://rolling-api.vercel.app/15-5';
+const BASE_URL = 'https://rolling-api.vercel.app/';
+const TEAM = '15-5';
 
-export async function getRecipientsData() {
+export async function getRecipientsData(id) {
 	try {
-		const response = await fetch(`${BASE_URL}/recipients/`);
+		const response = await fetch(`${BASE_URL}${TEAM}/recipients/${id}/`);
 		if (!response.ok) {
 			throw new Error(`HTTP error: ${response.status}`);
 		}
 		const data = await response.json();
+
 		return data;
 	} catch (error) {
 		console.error(error.message);
 	}
 }
 
-export async function getRecipientsMessage(userId) {
+export async function getRecipientsMessage(userId, offset, limit) {
 	try {
-		const response = await fetch(`${BASE_URL}/recipients/${userId}/messages`);
+		const response = await fetch(`${BASE_URL}${TEAM}/recipients/${userId}/messages/?offset=${offset}&limit=${limit}`);
 		if (!response.ok) {
 			throw new Error(`HTTP error: ${response.status}`);
 		}
 		const data = await response.json();
+		console.log(data);
+
 		return data;
 	} catch (error) {
 		console.error(error.message);
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,19 +2,39 @@ import { Link } from 'react-router-dom';
 import style from '../styles/Card.module.css';
 import plus from '/icons/plus.png';
 
-const Card = ({ userId }) => {
+const Card = ({ userId, id, sender, relationship, profileImg, cardFont, content, createdAt }) => {
+	const date = String(createdAt).replaceAll('-', '.');
+	const textIndex = date.indexOf('T', 0);
+	const createdDate = date.slice(0, textIndex);
+
 	return (
-		<div className={style.card}>
-			{userId ? (
-				<div className={style.card__link__button}>
-					<Link to={`/post/${userId}/message`}>
-						<img src={plus} alt='edit 페이지 이동 버튼' />
-					</Link>
+		<>
+			{!sender ? (
+				<div className={style.card}>
+					<div className={style.card__link__button}>
+						<Link to={`/post/${userId}/message`}>
+							<img src={plus} alt='edit 페이지 이동 버튼' />
+						</Link>
+					</div>
 				</div>
 			) : (
-				<div></div>
+				<div className={style.message__card}>
+					<div className={style.message__wrap} key={id} style={{ fontFamily: cardFont }}>
+						<div className={style.message__user}>
+							<img src={profileImg} alt='메세지 보낸 유저 이미지' />
+							<div>
+								<h3 className={style.user__name}>
+									From. <span>{sender}</span>
+								</h3>
+								<div className={style.user__relationship}>{relationship}</div>
+							</div>
+						</div>
+						<p className={style.content}>{content}</p>
+						<p className={style.createdAt}>{createdDate}</p>
+					</div>
+				</div>
 			)}
-		</div>
+		</>
 	);
 };
 export default Card;
