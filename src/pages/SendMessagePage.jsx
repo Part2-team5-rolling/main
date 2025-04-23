92f34fd0459efb27e4aaa798e10c33ffb61ef435
Habist <liasn@naver.com>
2025-04-23 16:07:12 +0900

    style : 메시지 전송 페이지 및 에디터 스타일 적용

--- src/pages/SendMessagePage.jsx
+++ src/pages/SendMessagePage.jsx
@@ -1,10 +1,15 @@
 import { useParams } from 'react-router-dom';
 import SendMessageForm from '../components/Form/SendMessageForm';
+import style from '../styles/Pages/SendMessagePage.module.css';
 
 function SendMessagePage() {
 	const { id } = useParams();
 	const reciipientId = id ?? Number(id);
-	return <>{isNaN(reciipientId) ? <div>error</div> : <SendMessageForm reciipientId={reciipientId} />}</>;
+	return (
+  <div className={style.content}>
+    {isNaN(reciipientId) ? <div>error</div> : <SendMessageForm reciipientId={reciipientId} />}
+  </div>
+  );
 }
 
 export default SendMessagePage;
