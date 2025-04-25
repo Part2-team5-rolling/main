1477cf4953d8bdd8c3d46c7029293d7ced94004d
Habist <liasn@naver.com>
2025-04-25 15:35:39 +0900

    feat : 카카오 공유하기 기능

--- src/components/Layout/post-components/ShareMenu.jsx
+++ src/components/Layout/post-components/ShareMenu.jsx
@@ -1,8 +1,9 @@
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import style from '../../../styles/ShareMenu.module.css';
 import { ToastContext } from '../../../hooks/ToastContextProvider';
 
-const ShareMenu = ({ onClick }) => {
+const { Kakao } = window;
+const ShareMenu = ({ onClick, commentCount }) => {
 	const currentUrl = window.location.href;
 	const { showToast } = useContext(ToastContext);
 
@@ -17,10 +18,44 @@ const ShareMenu = ({ onClick }) => {
 			});
 	};
 
+  const handleKakaoShare = () => {
+    const appURL = import.meta.env.VITE_KAKAO_APP_KEY;
+    const link = {
+      webUrl: appURL,
+    }
+    Kakao.Share.sendDefault({
+      objectType: 'feed',
+      content: {
+        title: '롤링페이퍼',
+        description: '#롤링페이퍼',
+        link,
+      },
+      social: {
+        commentCount,
+      },
+      buttons: [
+        {
+          title: '웹으로 보기',
+          link,
+        },
+      ],
+    });
+  };
+
+  useEffect(() => {
+    Kakao.cleanup();
+    const appKey = import.meta.env.VITE_KAKAO_APP_KEY; 
+    Kakao.init(appKey);
+    console.log(Kakao.isInitialized());
+  }, []);
+
 	return (
 		<>
 			<ul className={style.share__text__wrap}>
-				<li className={style.share__text} onClick={onClick}>
+				<li className={style.share__text} onClick={(e) => { 
+            handleKakaoShare();
+            onClick(e);
+          }}>
 					카카오톡 공유
 				</li>
 				<li
