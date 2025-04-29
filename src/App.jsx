9d75d289abfb041b45fa4dca5b551536156181af
Habist <liasn@naver.com>
2025-04-29 15:34:39 +0900

    feat : 에러 바운더리 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,19 +5,23 @@ import PostPage from './pages/PostPage';
 import SendMessagePage from './pages/SendMessagePage';
 import RollingPaperPage from './pages/RollingPaperPage';
 import EditPage from './pages/EditPage';
+import ErrorBoundary from './components/common/ErrorBoundary';
+import ErrorPage from './pages/ErrorPage';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/list" element={<ListPage />} />
-        <Route path='/post/:id' element={<PostPage />} />
-        <Route path='/post' element={<RollingPaperPage />} />
-        <Route path='/post/:id/message' element={<SendMessagePage />} />
-        <Route path='/post/:id/edit' element={<EditPage />} />
-      </Routes>
-    </BrowserRouter>
+    <ErrorBoundary fallback={<ErrorPage />}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/list" element={<ListPage />} />
+          <Route path='/post/:id' element={<PostPage />} />
+          <Route path='/post' element={<RollingPaperPage />} />
+          <Route path='/post/:id/message' element={<SendMessagePage />} />
+          <Route path='/post/:id/edit' element={<EditPage />} />
+        </Routes>
+      </BrowserRouter>
+    </ErrorBoundary>
   );
 }
 
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,10 @@
+
+function ErrorPage() {
+  return (
+  <>
+    오류가 발생했습니다!
+  </>
+  );
+}
+
+export default ErrorPage;
